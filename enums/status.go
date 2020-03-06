8b937cfc22523b26804332857feb1f83298f8701
Mysh! <mysh@tuta.io>
2020-03-06 11:50:57 +0800

    [add] Added the status of sales

--- enums/status.go
+++ enums/status.go
@@ -17,3 +17,16 @@ const (
 	Waiting
 	Closed
 )
+
+type StatusSales string
+
+const (
+	NewSale                  StatusSales = "new"
+	InvalidQR                StatusSales = "invalid_qr"
+	ReceiptInfoFound         StatusSales = "receipt_info_found"
+	ReceiptInfoNotFound      StatusSales = "receipt_info_not_found"
+	ReceiptInfoIsNotYetReady StatusSales = "receipt_info_is_not_yet_ready"
+	FailSale                 StatusSales = "fail"
+	PaymentsCreated          StatusSales = "payments_created"
+	PaymentsNotFound         StatusSales = "payments_not_found"
+)
