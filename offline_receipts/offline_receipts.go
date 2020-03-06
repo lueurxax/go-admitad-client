d6bbf2e68f4adb645a0ae2f479e9a8128db960a4
Mysh! <mysh@tuta.io>
2020-03-06 11:50:11 +0800

    [upd] Go Fmt Project

--- offline_receipts/offline_receipts.go
+++ offline_receipts/offline_receipts.go
@@ -1,64 +1,64 @@
 package offline_receipts
 
 import (
-    "fmt"
-    client "github.com/lueurxax/go-admitad-client"
-    "github.com/lueurxax/go-admitad-client/internal"
-    "github.com/lueurxax/go-admitad-client/requests"
-    "github.com/lueurxax/go-admitad-client/responses"
+	"fmt"
+	client "github.com/lueurxax/go-admitad-client"
+	"github.com/lueurxax/go-admitad-client/internal"
+	"github.com/lueurxax/go-admitad-client/requests"
+	"github.com/lueurxax/go-admitad-client/responses"
 )
 
 type OfflineReceipts struct {
-    *internal.BaseClient
+	*internal.BaseClient
 }
 
 // https://www.admitad.com/en/developers/doc/api_ru/methods/offline_sales/offline-receipts-get/
 func (r OfflineReceipts) Get(request requests.OfflineReceipts) (*responses.OfflineReceipts, error) {
-    if err := r.AutoRefresh(false); err != nil {
-        return nil, err
-    }
-    resp, err := r.get(request)
-    if err != nil {
-        if !client.IsAuthError(err) {
-            return nil, err
-        }
-        if err2 := r.AutoRefresh(true); err2 != nil {
-            return nil, fmt.Errorf("request err: %s, refresh token err: %s", err.Error(), err2.Error())
-        }
-        return r.get(request)
-    }
-    return resp, nil
+	if err := r.AutoRefresh(false); err != nil {
+		return nil, err
+	}
+	resp, err := r.get(request)
+	if err != nil {
+		if !client.IsAuthError(err) {
+			return nil, err
+		}
+		if err2 := r.AutoRefresh(true); err2 != nil {
+			return nil, fmt.Errorf("request err: %s, refresh token err: %s", err.Error(), err2.Error())
+		}
+		return r.get(request)
+	}
+	return resp, nil
 }
 
 // https://www.admitad.com/en/developers/doc/api_ru/methods/offline_sales/offline-receipts-get/
 func (r OfflineReceipts) get(request requests.OfflineReceipts) (*responses.OfflineReceipts, error) {
-    data, errResponse := new(responses.OfflineReceipts), new(responses.ErrorResponse)
+	data, errResponse := new(responses.OfflineReceipts), new(responses.ErrorResponse)
 
-    httpParams, logParams := request.Params()
+	httpParams, logParams := request.Params()
 
-    r.Logger.Debug("make request", "/offline_receipts/", logParams, nil)
+	r.Logger.Debug("make request", "/offline_receipts/", logParams, nil)
 
-    resp, err := r.R().
-        SetQueryParams(httpParams).
-        EnableTrace().
-        SetAuthToken(r.Auth.Token).
-        SetResult(data).
-        SetError(errResponse).
-        Get("/offline_receipts/")
+	resp, err := r.R().
+		SetQueryParams(httpParams).
+		EnableTrace().
+		SetAuthToken(r.Auth.Token).
+		SetResult(data).
+		SetError(errResponse).
+		Get("/offline_receipts/")
 
-    if err != nil {
-        r.Logger.Error("http error", "/offline_receipts/", logParams, err)
-        return nil, err
-    }
+	if err != nil {
+		r.Logger.Error("http error", "/offline_receipts/", logParams, err)
+		return nil, err
+	}
 
-    r.Metrics.Collect("/offline_receipts/", resp.StatusCode(), errResponse.StatusCode, resp.Time())
+	r.Metrics.Collect("/offline_receipts/", resp.StatusCode(), errResponse.StatusCode, resp.Time())
 
-    if resp.Error() != nil {
-        r.Logger.Error("app error", "/offline_receipts/", errResponse.ErrLogParams(logParams), errResponse)
-        return nil, errResponse
-    }
+	if resp.Error() != nil {
+		r.Logger.Error("app error", "/offline_receipts/", errResponse.ErrLogParams(logParams), errResponse)
+		return nil, errResponse
+	}
 
-    r.Logger.Debug("success request", "/offline_receipts/", logParams, nil)
+	r.Logger.Debug("success request", "/offline_receipts/", logParams, nil)
 
-    return data, nil
+	return data, nil
 }
--- requests/offline_receipts.go
+++ requests/offline_receipts.go
@@ -1,73 +1,73 @@
 package requests
 
 import (
-    "strconv"
-    "time"
+	"strconv"
+	"time"
 )
 
 type OfflineReceipts struct {
-    WebsiteID *int
-    ID string
-    DatetimeCreatedFrom *time.Time
-    DatetimeCreatedTo *time.Time
-    DateTimeUpdatedFrom *time.Time
-    DateTimeUpdatedTo *time.Time
-    Limit int
-    Offset *int
-    PurchaserID string
+	WebsiteID           *int
+	ID                  string
+	DatetimeCreatedFrom *time.Time
+	DatetimeCreatedTo   *time.Time
+	DateTimeUpdatedFrom *time.Time
+	DateTimeUpdatedTo   *time.Time
+	Limit               int
+	Offset              *int
+	PurchaserID         string
 }
 
 func (r *OfflineReceipts) Params() (map[string]string, map[string]interface{}) {
-    params := map[string]string{}
-    logParams := map[string]interface{}{}
-    if r == nil {
-        return params, logParams
-    }
+	params := map[string]string{}
+	logParams := map[string]interface{}{}
+	if r == nil {
+		return params, logParams
+	}
 
-    if r.WebsiteID != nil {
-        logParams["website_id"] = r.WebsiteID
-        params["website_id"] = string(*r.WebsiteID)
-    }
+	if r.WebsiteID != nil {
+		logParams["website_id"] = r.WebsiteID
+		params["website_id"] = string(*r.WebsiteID)
+	}
 
-    if r.ID != "" {
-        logParams["id"] = r.ID
-        params["id"] = r.ID
-    }
+	if r.ID != "" {
+		logParams["id"] = r.ID
+		params["id"] = r.ID
+	}
 
-    if r.Limit != 0 {
-        logParams["limit"] = r.ID
-        params["limit"] = strconv.Itoa(r.Limit)
-    }
+	if r.Limit != 0 {
+		logParams["limit"] = r.ID
+		params["limit"] = strconv.Itoa(r.Limit)
+	}
 
-    if r.Offset != nil {
-        logParams["offset"] = r.Offset
-        params["offset"] = strconv.Itoa(*r.Offset)
-    }
+	if r.Offset != nil {
+		logParams["offset"] = r.Offset
+		params["offset"] = strconv.Itoa(*r.Offset)
+	}
 
-    if r.PurchaserID != "" {
-        logParams["purchaser_id"] = r.PurchaserID
-        params["purchaser_id"] = r.PurchaserID
-    }
+	if r.PurchaserID != "" {
+		logParams["purchaser_id"] = r.PurchaserID
+		params["purchaser_id"] = r.PurchaserID
+	}
 
-    if r.DatetimeCreatedFrom != nil {
-        logParams["datetime_created_from"] = r.DatetimeCreatedFrom
-        params["datetime_created_from"] = r.DatetimeCreatedFrom.Format("02.01.2006 15:04")
-    }
+	if r.DatetimeCreatedFrom != nil {
+		logParams["datetime_created_from"] = r.DatetimeCreatedFrom
+		params["datetime_created_from"] = r.DatetimeCreatedFrom.Format("02.01.2006 15:04")
+	}
 
-    if r.DatetimeCreatedTo != nil {
-        logParams["datetime_created_to"] = r.DatetimeCreatedTo
-        params["datetime_created_to"] = r.DatetimeCreatedTo.Format("02.01.2006 15:04")
-    }
+	if r.DatetimeCreatedTo != nil {
+		logParams["datetime_created_to"] = r.DatetimeCreatedTo
+		params["datetime_created_to"] = r.DatetimeCreatedTo.Format("02.01.2006 15:04")
+	}
 
-    if r.DateTimeUpdatedFrom != nil {
-        logParams["datetime_updated_from"] = r.DateTimeUpdatedFrom
-        params["datetime_updated_from"] = r.DateTimeUpdatedFrom.Format("02.01.2006 15:04")
-    }
+	if r.DateTimeUpdatedFrom != nil {
+		logParams["datetime_updated_from"] = r.DateTimeUpdatedFrom
+		params["datetime_updated_from"] = r.DateTimeUpdatedFrom.Format("02.01.2006 15:04")
+	}
 
-    if r.DateTimeUpdatedTo != nil {
-        logParams["datetime_updated_to"] = r.DateTimeUpdatedTo
-        params["datetime_updated_to"] = r.DateTimeUpdatedTo.Format("02.01.2006 15:04")
-    }
+	if r.DateTimeUpdatedTo != nil {
+		logParams["datetime_updated_to"] = r.DateTimeUpdatedTo
+		params["datetime_updated_to"] = r.DateTimeUpdatedTo.Format("02.01.2006 15:04")
+	}
 
-    return params, logParams
+	return params, logParams
 }
